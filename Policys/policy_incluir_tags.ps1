69b5a5d6984126310ab0cf45e610603d77f9851a
Carascoza <mcarascoza@gmail.com>
2024-11-28 14:49:44 -0300

    Update

--- Policys/policy_incluir_tags.ps1
+++ Policys/policy_incluir_tags.ps1
@@ -17,11 +17,9 @@ $LogTime = Get-Date -Format "dd-MM-yyyy;hh:mm:ss"
 $Time = Get-Date -Format "MM-dd-yyyy"
 $LogFile = "<caminho>\policy_tags_" + $Time + ".log"
 $subscriptionId = "sua_assinatura"
-$resourceGroupName = "seu_grupo_de_recursos"
-$identityName = "seu_nome_de_identidade"
 $policyName = "apply-default-tags"
 $policyDisplayName = "Apply Default Tags"
-$policyDescription = "Applies default tags to all newly created resources in East US 2."
+$policyDescription = "Applies default tags to Resource Groups, Virtual Machines, VNets, and Storage Accounts in East US 2."
 $roleDefinitionId = "b24988ac-6180-42a0-ab88-20f7382dd24c" # ID da função "Contributor"
 
 #Cria arquivo log
@@ -29,29 +27,17 @@ $roleDefinitionId = "b24988ac-6180-42a0-ab88-20f7382dd24c" # ID da função "Con
 "Inicio;" + $LogTime | Out-File $LogFile -Append -Force
 
 Try { 
-
-
     #conectar na azure tenant nome_empresa
     "Conctar na azure;" + $LogTime | Out-File $LogFile -Append -Force
     Connect-AzAccount
     Set-AzContext -SubscriptionId $subscriptionId
-
 }
 
 Catch {
-
     $ErrorMessage = $_.Exception.Message
     "Error Concetar na azure;" + $ErrorMessage | Out-File $LogFile -Append -Force
     Write-Host -BackgroundColor red -ForegroundColor Black -Object $ErrorMessage
     exit
-}
-
-
-# Criar uma nova identidade gerida (se não existir)
-if (-not (Get-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name $identityName -ErrorAction SilentlyContinue)) {
-    $identity = New-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name $identityName
-} else {
-    $identity = Get-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name $identityName
 }
 
 # Definir a política JSON corretamente
@@ -59,12 +45,28 @@ $policyRule = @{
   "if" = @{
     "allOf" = @(
       @{
-        "field" = "type"
-        "equals" = "Microsoft.Resources/subscriptions/resourceGroups"
-      },
-      @{
         "field" = "location"
         "equals" = "eastus2"
+      },
+      @{
+        "anyOf" = @(
+          @{
+            "field" = "type"
+            "equals" = "Microsoft.Resources/subscriptions/resourceGroups"
+          },
+          @{
+            "field" = "type"
+            "equals" = "Microsoft.Compute/virtualMachines"
+          },
+          @{
+            "field" = "type"
+            "equals" = "Microsoft.Network/virtualNetworks"
+          },
+          @{
+            "field" = "type"
+            "equals" = "Microsoft.Storage/storageAccounts"
+          }
+        )
       }
     )
   }
@@ -83,7 +85,7 @@ $policyRule = @{
         @{
           "operation" = "add"
           "field" = "tags.owner"
-          "value" = "carascoza"
+          "value" = "owner"
         }
         @{
           "operation" = "add"
@@ -101,7 +103,7 @@ $policyJson = $policyRule | ConvertTo-Json -Depth 10
 # Criar a definição da política
 $policyDefinition = New-AzPolicyDefinition -Name $policyName -DisplayName $policyDisplayName -Description $policyDescription -Policy $policyJson
 
-# Atribuir a política à assinatura com a identidade gerida
-New-AzPolicyAssignment -Name $policyName -PolicyDefinition $policyDefinition -Scope "/subscriptions/$subscriptionId" -Identity $identity
+# Atribuir a política à assinatura
+New-AzPolicyAssignment -Name $policyName -PolicyDefinition $policyDefinition -Scope "/subscriptions/$subscriptionId"
 
 Write-Output "Política de tags padrão criada e atribuída com sucesso!"
